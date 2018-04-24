d5f56362aa012342046d1910c28f3d5d051493a5
altamont <greg@altamonttech.com>
2018-04-24 11:41:26 -0400

    crude oil net exports

--- data_script.R
+++ data_script.R
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 # Description -------------------------------------------------------------
 
 # We'll fill this in later with comments....
@@ -8,7 +6,7 @@
 
 
 # R Packages --------------------------------------------------------------
-
+library(tidyverse)
 
 
 
@@ -16,8 +14,30 @@
 # Load Data ---------------------------------------------------------------
 
 energy.data <- read.csv("data/01330006-eng.csv")
-=======
-#some hypo code from greg!!!!
 
-#more code
->>>>>>> e7c094f4e4e42d5a25a0a5ef49a758652ba8307d
+
+#calculate the net exports
+
+crude.oil.barrels.ne <- energy.data %>%
+  select(-c(Vector, Coordinate)) %>%
+  filter(STA == "Barrels") %>%
+  filter(PRO == "Crude oil and equivalent products") %>%
+  spread(key = PIP, value = Value) %>%
+  mutate(Net.Exports = Exports - Imports) %>%
+  group_by(Ref_Date, GEO) %>%
+  summarise(Total.Net.Exports = sum(Net.Exports))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
